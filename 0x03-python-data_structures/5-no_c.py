910f8f1de5f2f45b41417f69f3a15f7815794a58
YaraAminn <aminyara.94@gmail.com>
2023-12-13 12:51:31 -0800

    task55

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,8 +2,6 @@
 def no_c(my_string):
     string = ""
     for i in range(len(my_string)):
-        if my_string[i] == 'c' or my_string[i] == 'C':
-            continue
-        else:
+        if my_string[i] != 'c' and my_string[i] != 'C':
             string += my_string[i]
     return string
